8e72012c9a55b23dc47343067e5feac05ea0c9a3
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2020-04-23 17:21:59 -0300

    search terminada

--- Hernandez-Zarate-TP1.hs
+++ Hernandez-Zarate-TP1.hs
@@ -12,30 +12,30 @@ data TTree k v = Node k (Maybe v ) (TTree k v ) (TTree k v ) (TTree k v )
                 | E 
                 deriving Show
 
-t = Node "r" Nothing E 
-					(Node "e" (Just 16) (Node "a" Nothing E (Leaf "s" 1) E)
-                                        (Node "o" (Just 2) (Leaf "d" 9)
+t = Node 'r' Nothing E 
+                    (Node 'e' (Just 16) (Node 'a' Nothing E (Leaf 's' 1) E)
+                                        (Node 'o' (Just 2) (Leaf 'd' 9)
                                                              E
-                                                            (Leaf "s" 4))
+                                                            (Leaf 's' 4))
                                         E)
-                    (Node "s" Nothing E (Node "i" (Just 4)  (Leaf "e" 8)
-                                                            (Leaf "n" 7)
+                    (Node 's' Nothing E (Node 'i' (Just 4)  (Leaf 'e' 8)
+                                                            (Leaf 'n' 7)
                                                              E)
                                         E)
 
 --devuelve el valor asociado a una clave.
--- search "sin" t = 7
+-- search 'sin' t = 7
 
 search :: Ord k => [k] -> TTree k v -> Maybe v
 search _ E = error "El arbol está vacío"
-search (x:y:xs) (Leaf k v) = "La palabra buscada no está en el dic"
-search [x] (Leaf k v) = if (x = k) then v else error "La palabra buscada no está en el dic"
-search [x] (Node k v i m d) = if (k = x) 	then v
-											else 
-												if(x > k) search [x] d else search x i
-search (x:xs) (Node k v i m d) = if (x < k) 	then search (x:xs) i
-												else if (x>k) 	then search (x:xs) d
-																else search xs m
+search (x:y:xs) (Leaf k v) = error "La palabra buscada no está en el dic"
+search (x: []) (Leaf k v) = if (x == k) then (Just v) else error "La palabra buscada no está en el dic"
+search (x:[]) (Node k v i m d) = if (k == x)    then v
+                                                else 
+                                                    if(x > k) then search (x:[]) d else search (x:[]) i
+search (x:xs) (Node k v i m d) = if (x < k) then search (x:xs) i
+                                            else if (x>k)   then search (x:xs) d
+                                                            else search xs m
 
 insert :: Ord k => [k] -> v -> TTree k v -> TTree k v
 insert = undefined
